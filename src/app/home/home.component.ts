319b6198c33854a819eb82214cd183f1bee1eb6f
BoomBakar <abubakardar608@gmail.com>
2023-08-24 12:49:48 +0500

    home component completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title:string = 'Home';
+  constructor() { }
+  name:string = '';
+  email:string = '';
+  message:string = '';
+  isSubmitted:boolean = false;
+  data:Array<any> = [];
+
+  onSubmit():void {
+    this.isSubmitted = true;
+    this.data.push({
+      name: this.name,
+      email: this.email,
+      message: this.message
+    });
+    console.log(`Thank you for your message! ${this.name} ${this.email} ${this.message}`);
+  }
 }
